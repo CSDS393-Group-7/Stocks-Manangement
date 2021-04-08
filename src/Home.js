d1c4d79fd38033cecee8dc89eed574942ea147e0
Vivian <nhl16@case.edu>
2021-04-08 13:15:38 -0400

    basic function chart complete

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Chart from './Chart.js';
 import "./Home.css";
+
 
 const Home = () => {
     return (
         <div>
             <h1>Home page</h1>
+            <Chart/>
+
         </div>
+
     );
 };
 
