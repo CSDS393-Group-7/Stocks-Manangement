83ae4e399ce6060bf0716d6e871beb9ec764df7b
Trung Nguyen <rezolden@gmail.com>
2021-04-29 06:30:39 -0400

    C:/Program Files/Git/api/news/get-list & search added

--- backend/classes/News.js
+++ backend/classes/News.js
@@ -7,40 +7,69 @@ const ObjectID = require('mongodb').ObjectID;
 const db = require('../database');
 
 class News {
+    static PULL_DATA_COUNTER = 0;
+
     static async pullDataFromProviders() {
-        const url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list';
-        fetch(url, { 
+        console.log(`[NEWS]: Starting to pull data | ${++(News.PULL_DATA_COUNTER)} times`);
+        const url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list?snippetCount=40';
+        await fetch(url, { 
             method: "POST",
             headers: {
                 "x-rapidapi-key": process.env.YAHOO_FIN_APIKEY,
-                "x-rapidapi-host": process.env.YAHOO_FIN_HOST
+                "x-rapidapi-host": process.env.YAHOO_FIN_HOST,
+                "useQueryString": true,
             }
         })
         .then(res => res.json())
         .then(data => data['data']['main']['stream'])
         .then(newsList => {
             const formatted = [];
+            console.log(`[NEWS]: Before filtering: ${newsList.length}`);
 
             newsList.forEach(news => {
                 const content = news['content'];
 
-                if (content['clickThroughUrl'] === null || content['thumbnail'] === null)
-                    return;
+                try {
+                    const insertData = {
+                        _id: content['id'],
+                        title: content['title'],
+                        contentType: content['contentType'] || "NEWS",
+                        thumbnailUrl: content['thumbnail']['resolutions'].slice(-1)[0]['url'],
+                        originalUrl: content['clickThroughUrl']['url'],
+                        provider: content['provider']['displayName'],
+                        publicationDate: new Date(content['pubDate']),
+                    };
+    
+                    formatted.push(insertData);
+                }
+                catch (err) { return; }
 
-                const insertData = {
-                    _id: content['id'],
-                    title: content['title'],
-                    contentType: content['contentType'] || "NEWS",
-                    thumbnailUrl: content['thumbnail']['resolutions'].slice(-1)[0]['url'],
-                    originalUrl: content['clickThroughUrl']['url'],
-                    provider: content['provider']['displayName'],
-                    publicationDate: content['pubDate'],
-                };
-
-                formatted.push(insertData);
             });
+            
             db.collection('news').insertMany(formatted, { ordered: false });
+            console.log(`[NEWS]: After filtering: ${formatted.length}`);
         });
+        
+        console.log(`[NEWS]: Successfully pulled data`);
+    }
+
+    static async getNewsList() {
+        const limit = Number(process.env.NEWS_LIMIT);
+        return await db.collection('news').find()
+            .sort({publicationDate: -1})
+            .limit(limit)
+            .toArray();
+    }
+
+    static async search(keyword) {
+        const limit = Number(process.env.NEWS_LIMIT);
+        return await db.collection('news').find(
+            { $text: { $search: keyword }},
+            { score: { $meta: "textScore" }} 
+        )
+        .sort({ score: { $meta: "textScore" } })
+        .limit(limit)
+        .toArray();
     }
 };
 
--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ db.connect('mongodb://localhost:27017/stock-management', (err) => {
 
     app.use('/api/user', user);
     app.use('/api/news', news);
-
+    
     app.listen(port, () => {
         console.log(`Successfully started server! Listening at port ${port}`);
     });
