a6f3603a273077bbb56af44b60197a743b2b7638
Trung Nguyen <rezolden@gmail.com>
2021-05-02 09:13:58 -0400

    update avatar initial + backend news

--- frontend/src/components/NavBar/UserCard.js
+++ frontend/src/components/NavBar/UserCard.js
@@ -24,7 +24,7 @@ function UserCard(props) {
 
   return (
     <div className={classes.root}>
-      <Avatar className={classes.avatar} alt="Admin">M</Avatar>
+      <Avatar className={classes.avatar} alt="Admin">{props.name[0].toUpperCase()}</Avatar>
       <div className={classes.info}>
         <Typography variant="subtitle2">{props.name}</Typography>
         <Typography variant="body2">{props.role}</Typography>
