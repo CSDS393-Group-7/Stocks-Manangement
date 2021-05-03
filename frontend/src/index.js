74521c2f8b42764986c6c1967c4d8a1067c51cb5
Kris Tran <tungtran2222@gmail.com>
2021-05-03 23:11:33 +0700

    global save

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,16 +7,29 @@ import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import userApp from './store/user/user';
 
-const store = createStore(userApp);
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, userApp)
+ 
+let store = createStore(persistedReducer)
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
