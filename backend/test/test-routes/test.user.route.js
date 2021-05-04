7b4913c6ac222a94174f9bcfccc83e9c1b7bc324
Nguyenxloc <nguyenxloc@Nguyenxlocs-MacBook-Pro.local>
2021-05-05 04:16:12 +0700

    Finish test.user.route

--- backend/test/test-routes/test.user.route.js
+++ backend/test/test-routes/test.user.route.js
@@ -5,43 +5,103 @@ const should = chai.should();
 const db = require('../../database');
 
 require('dotenv').config();
-
-
-
+const userExisted = {
+    "username":"locdeptrai",
+    "password":"loclocloc",
+    "fullName":"loc",
+    "email":"123@gmail.com"
+};
+const newUser = {
+    "username":"testing_not_existed",
+    "password":"123",
+    "fullName": "thanh tung",
+    "email":"tungtt@gmail.com"
+};
+const userToLogin = {
+    "username":"testing_tung",
+    "password":"123"
+};
+const userWrongPassword = {
+    "username":"testing_tung",
+    "password":"123456"
+}
+const userDoesntExist = {
+    "username":"loc",
+    "password":"123456"
+}
 
 describe("Test User Create Route", (done) => {
-    const userToCreate = {"username":"testing_tung","password":"123", "fullName": "thanh tung","email":"tungtt@gmail.com"};
-    it("Test create user",  (done) => {
+    it("Return 409- user already existed",  (done) => {
         db.connect(process.env.DATABASE, (err) => {
             if (err)
                 throw err;
 
             request(rest_app)
             .post('/api/user/create')
-            .send(userToCreate)
-            .expect(200)
-            .then(res => {
-                res.body.should.be.an('string');
-                done();
-            }).catch(done);
+            .send(userExisted)
+            .expect(409,done);
+        });
+    });
+
+    it('Return 200- user created', function (done) {
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+
+            request(rest_app)
+                .post('/api/user/create')
+                .send(newUser)
+                .expect(200, done);
         });
     });
 });
 
 describe("Test User Login Route", (done) => {
-    const userToLogin = {"username":"locdeptrai","password":"loclocloc"};
-    it("Test login user",  (done) => {
+    it("Return 200 - Successfully Login",  (done) => {
         db.connect(process.env.DATABASE, (err) => {
             if (err)
                 throw err;
             request(rest_app)
                 .post('/api/user/login')
                 .send(userToLogin)
-                .expect(200)
-                .then(res => {
-                    res.body.should.be.an('string');
-                    done();
-                }).catch(done);
+                .expect(200,done);
+        });
+    });
+
+    it("Return 403 - Wrong Password But User Exist",  (done) => {
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+            request(rest_app)
+                .post('/api/user/login')
+                .send(userWrongPassword)
+                .expect(403,done);
+        });
+    });
+
+    it("Return 404 - User doesn't exist",  (done) => {
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+
+            request(rest_app)
+                .post('/api/user/login')
+                .send(userDoesntExist)
+                .expect(404,done);
+        });
+    });
+});
+
+describe("Test Get Watch List Route", (done) =>{
+    it('Return 200 - Successfully Get The Watch List and Authorized Token', function () {
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+
+            request(rest_app)
+                .get('/api/user/watchList')
+                .set("Authorization", "BEARER eyJhbGciOiJIUzI1NiJ9.dGVzdGluZ190cnVuZ18y.OckHqtlgjEZv4id-lywgI5l0PW0oRhNZMW8yQ4sHsdM")
+                .expect(200,done);
         });
     });
 });
