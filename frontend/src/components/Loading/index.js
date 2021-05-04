bdf7a76629649df8add285735cfb57aaba7b65a8
Trung Nguyen <rezolden@gmail.com>
2021-05-04 13:34:00 -0400

    sync loading

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -1,13 +1,27 @@
 import { Backdrop, useTheme } from '@material-ui/core';
 import { PacmanLoader } from 'react-spinners';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Loading({ open }) {
+/**
+ * Loading Backdrop transition
+ * @param {boolean} open      Whether the loading shows up or not
+ * @param {number}  delayed   (optional) The period of time the loading stays after open === false
+ */
+function Loading({ open, delayed }) {
   const theme = useTheme();
 
+  const [trueOpen, setTrueOpen] = useState(true);
+
+  useEffect(() => {
+    if (open || !delayed)
+      setTrueOpen(open);
+    else
+      setTimeout(() => setTrueOpen(open), delayed)
+  }, open)
+
   return (
-    <Backdrop open={open}>
-      <PacmanLoader color={theme.palette.primary.main} size={25} />
+    <Backdrop open={trueOpen} transitionDuration={500} style={{zIndex: 100000, backgroundColor: 'rgb(0 0 0 / 50%)'}}>
+      <PacmanLoader color={theme.palette.primary.light} size={27} />
     </Backdrop>
   )
 }
