2c4db3c9d17d555f2d9ca82ef0945786155ec699
Trung Nguyen <rezolden@gmail.com>
2021-05-04 12:22:52 -0400

    added Alert

---
+++ frontend/src/components/Alert/ErrorAlert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Alert from '@material-ui/lab/Alert';
+import { Snackbar } from '@material-ui/core';
+
+/**
+ * Success Alert
+ * @param {JSX | string}  message   The error message
+ * @param {boolean}       open      Whether the alert shows
+ * @param {Function}      onClose   Takes in <pre>event</pre> and <pre>reason</pre>. 
+ * @returns 
+ */
+function ErrorAlert({ message, open, onClose }) {
+  return (
+    <Snackbar open={open} onClose={onClose}>
+      <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+        {message}
+      </Alert>
+    </Snackbar>
+  );  
+}
+
+export default ErrorAlert;
---
+++ frontend/src/components/Alert/SuccessAlert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Alert from '@material-ui/lab/Alert';
+import { Snackbar } from '@material-ui/core';
+
+/**
+ * Success Alert
+ * @param {JSX | string}  message   The success message
+ * @param {boolean}       open      Whether the alert shows
+ * @param {Function}      onClose   Takes in <pre>event</pre> and <pre>reason</pre>. 
+ * @returns 
+ */
+function SuccessAlert({ message, open, onClose }) {
+  return (
+    <Snackbar open={open} onClose={onClose}>
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        {message}
+      </Alert>
+    </Snackbar>
+  );  
+}
+
+export default SuccessAlert;
