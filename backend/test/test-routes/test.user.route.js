750449ef7c12c5437adc945fdfb31ce5a7fd78ae
Trung Nguyen <rezolden@gmail.com>
2021-05-04 16:18:20 -0400

    fix conflict db:

--- backend/test/test-routes/test.user.route.js
+++ backend/test/test-routes/test.user.route.js
@@ -1,12 +1,22 @@
 const request = require("supertest");
 const rest_app = require('../../server/rest_server.js');
 const chai = require("chai");
-const should = chai.should();
+const should = chai.should(); 
+const db = require('../../database');
+
+require('dotenv').config();
+
+
+
 
 describe("Test User Create Route", (done) => {
     const userToCreate = {"username":"testing_tung","password":"123", "fullName": "thanh tung","email":"tungtt@gmail.com"};
     it("Test create user",  (done) => {
-         request(rest_app)
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+
+            request(rest_app)
             .post('/api/user/create')
             .send(userToCreate)
             .expect(200)
@@ -14,19 +24,24 @@ describe("Test User Create Route", (done) => {
                 res.body.should.be.an('string');
                 done();
             }).catch(done);
+        });
     });
 });
 
 describe("Test User Login Route", (done) => {
     const userToLogin = {"username":"locdeptrai","password":"loclocloc"};
     it("Test login user",  (done) => {
-        request(rest_app)
-            .post('/api/user/login')
-            .send(userToLogin)
-            .expect(200)
-            .then(res => {
-                res.body.should.be.an('string');
-                done();
-            }).catch(done);
+        db.connect(process.env.DATABASE, (err) => {
+            if (err)
+                throw err;
+            request(rest_app)
+                .post('/api/user/login')
+                .send(userToLogin)
+                .expect(200)
+                .then(res => {
+                    res.body.should.be.an('string');
+                    done();
+                }).catch(done);
+        });
     });
 });
